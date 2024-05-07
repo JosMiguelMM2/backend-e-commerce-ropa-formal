b2aeec684695dfb1b475febef463a6ca103147b2
JosMiguelMM2 <josemmendez@ucundinamarca.edu.co>
2024-05-06 19:06:51 -0500

    Cambios en cors

--- src/main/kotlin/com/ropaformal/ecommerceropaformal/web/config/CorsConfig.kt
+++ src/main/kotlin/com/ropaformal/ecommerceropaformal/web/config/CorsConfig.kt
@@ -24,7 +24,7 @@ class CorsConfig {
   fun corsConfigurationSource(): CorsConfigurationSource {
     val corsConfiguration = CorsConfiguration()
     corsConfiguration.setAllowedOriginPatterns(
-      listOf("*")
+      listOf("http://localhost:4000", "https://formal-wear-ecommerce.vercel.app")
     ) //ruta a las cuales se concede acceso
     corsConfiguration.allowedMethods = listOf("GET", "POST", "PUT", "DELETE", "PATCH") // metodos permitidos
     corsConfiguration.allowedHeaders = listOf("*", "Authorization") //acceso a los headers para tomar el token
